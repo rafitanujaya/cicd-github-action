44ec97f62dc05d3727e81d1410c814e17fff84a3
rafi tanujaya <rafibussines@gmail.com>
2025-12-06 22:24:28 +0700

    test: adding create user test

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,3 +1,34 @@
-describe('', () => {
-    
+import supertest from "supertest";
+import { app } from "../src/server.js";
+
+describe('POST /api/users', () => {
+    it('should can create user', async () => {
+        const result = await supertest(app).post('/api/users').send({
+            name: 'user-1',
+            age: 19
+        })
+
+        expect(result.status).toBe(201)
+        expect(result.body.data.id).toBeDefined()
+        expect(result.body.data.name).toBe('user-1')
+        expect(result.body.data.age).toBe(19)
+    });
+
+    it('should can reject request is property name missing', async () => {
+        const result = await supertest(app).post('/api/users').send({
+            age: 19
+        })
+
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBe('Nama wajib ada')
+    })
+
+    it('should can reject request is property age missing', async () => {
+        const result = await supertest(app).post('/api/users').send({
+            name: 'user-1'
+        })
+
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBe('Umur wajib ada')
+    })
 });
